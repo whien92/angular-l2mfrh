1d38ae805748e87e437623bca16c60d2b400af17
Hien Vu <vvhien@tma.com.vn>
2019-08-19 16:20:34 +0700

    managing data

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -13,7 +15,13 @@ export class ProductDetailsComponent implements OnInit {
   product;
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
+
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
